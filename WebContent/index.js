17f2e856840a44f11748fd9d4f49b22b8af7893d
muninnhugin <muninnhugin@gmail.com>
2022-10-21 13:26:25 -0700

    add browse by genre to index page

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,5 +1,6 @@
 let search_form = jQuery("#search_form");
-//let genre_form = jQuery("#genre_browse_form");
+let genre_list = jQuery("#genre_browse_list");
+
 
 function handleSearchSubmit(searchEvent)
 {
@@ -11,31 +12,30 @@ function handleSearchSubmit(searchEvent)
     window.location.assign(redirectUrl);
 }
 
+// TODO populate genre form with data from BrowseListServlet
+function populateGenreBrowseList(genreListData)
+{
+    for(let i = 0; i < genreListData.length; ++i)
+    {
+        let genre_id = genreListData[i]["genre_id"];
+        let genre_name = genreListData[i]["genre_name"];
+        let genreHtml = '<div class="genre_item"><a href="movie-list.html?genre_id=' +
+            genre_id + '">' + genre_name + '</a></div>';
+        genre_list.append(genreHtml);
+    }
+}
+
 search_form.submit(handleSearchSubmit);
 
-
-
 // requests list of genres
-// jQuery.ajax({
-//     dataType: "json", // Setting return data type
-//     method: "GET", // Setting request method
-//     url: "api/browse-list", // Setting request url, which is mapped by MovieListServlet in MovieListServlet.java
-//     success: (genreListData) => populateGenreBrowseForm(genreListData) // Setting callback function to handle data returned successfully by the StarsServlet
-// });
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/browse-list", // Setting request url, which is mapped by MovieListServlet in MovieListServlet.java
+    success: (genreListData) => populateGenreBrowseList(genreListData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
 
-// // TODO populate genre form with data from BrowseListServlet
-// function populateGenreBrowseForm(genreListData)
-// {
-//     for(let i = 0; i < genreListData.length; ++i)
-//     {
-//         let genre_id = genreListData[i]["genre_id"];
-//         let genre_name = genreListData[i]["genre_name"];
-//         let genreHtml = "<label><input type='radio' name='genre_id' value='" +
-//             genre_id + "'>" + genre_name + "</label><br>";
-//         genre_form.append(genreHtml);
-//     }
-//     genre_form.append("<input type='submit' value='Browse'>");
-// }
+
 
 // // TODO handle genre browse submit event
 // function handleGenreBrowseSubmit(genreBrowseEvent)
