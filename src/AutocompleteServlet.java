e6b06c7b585e9fd30d42b4e7cae9b7cbe9f1a930
muninnhugin <muninnhugin@gmail.com>
2022-11-15 19:43:14 -0800

    make changes to autocomplete servlet

--- src/AutocompleteServlet.java
+++ src/AutocompleteServlet.java
@@ -34,21 +34,33 @@ public class AutocompleteServlet extends HttpServlet {
 
             String query = request.getParameter("query");
             String[] tokens = query.split(" ");
-            String statementStr = "SELECT id, title\n" +
-                    "FROM movies\n" +
-                    "WHERE MATCH(title) AGAINST('";
-            for (String token : tokens) {
-                statementStr += "+" + token + "* ";
+            String statementStr = null;
+
+            if(tokens.length > 1) {
+                 statementStr = "SELECT id, title\n" +
+                        "FROM movies\n" +
+                        "WHERE MATCH(title) AGAINST('";
+                for (String token : tokens) {
+                    statementStr += "+" + token + "* ";
+                }
+                statementStr += "')" + "" +
+                        "LIMIT " + MAX_SUGGESTIONS;
             }
-            statementStr += "')";
+            else
+            {
+                statementStr = "SELECT id, title\n" +
+                        "FROM movies\n" +
+                        "WHERE MATCH(title) AGAINST('" + tokens[0] + "') " +
+                        "OR MATCH(title) AGAINST('" + tokens[0] + "*' IN BOOLEAN MODE) \n" +
+                        "LIMIT " + MAX_SUGGESTIONS;
+            }
+
 
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(statementStr);
 
-            int numSuggestions = 0;
             MovieTitleAutocomplete movieTitleAutocomplete = new MovieTitleAutocomplete(query);
-            while (resultSet.next() && numSuggestions < MAX_SUGGESTIONS) {
-                ++numSuggestions;
+            while (resultSet.next()) {
                 movieTitleAutocomplete.addMovie(resultSet);
             }
 
